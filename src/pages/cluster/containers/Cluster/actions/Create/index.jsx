c56ac292e6514b871f474de903b6a2ab5eb99ad7
liuying <liu.ying@99cloud.net>
2023-03-24 09:47:01 +0800

    fix: fix externaldomain for create cluster
    Signed-off-by: liuying <liu.ying@99cloud.net>

--- src/pages/cluster/containers/Cluster/actions/Create/index.jsx
+++ src/pages/cluster/containers/Cluster/actions/Create/index.jsx
@@ -237,7 +237,7 @@ export default class Create extends StepAction {
       ? { 'kubeclipper.io/externalPort': String(externalIP.port) }
       : {};
 
-    const [domain, domainPort] = externalDomain.split(':');
+    const [domain, domainPort] = (externalDomain || '').split(':');
     const externalDomainlabel = domain
       ? { 'kubeclipper.io/externalDomain': domain }
       : {};
